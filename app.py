1f9883aae2511d68074d337067fdefb810ef6b8e
Aleksey Vesyoliy <43518301+pirrencode@users.noreply.github.com>
2024-09-18 17:56:26 +0300

    Fix

--- app.py
+++ app.py
@@ -9,16 +9,23 @@ st.title("Stock Prices Data Viewer v0.0.1")
 # Snowflake connection details
 @st.cache_resource
 def create_snowflake_connection():
-    conn = snowflake.connector.connect(
-        user=st.secrets["snowflake"]["user"],
-        password=st.secrets["snowflake"]["password"],
-        account=st.secrets["snowflake"]["account"],
-        role=st.secrets["snowflake"]["role"],
-        warehouse=st.secrets["snowflake"]["warehouse"],
-        database=st.secrets["snowflake"]["database"],
-        schema=st.secrets["snowflake"]["schema"]
-    )
-    return conn
+    try:
+        conn = snowflake.connector.connect(
+            user=st.secrets["snowflake"]["user"],
+            password=st.secrets["snowflake"]["password"],
+            account=st.secrets["snowflake"]["account"],
+            role=st.secrets["snowflake"]["role"],
+            warehouse=st.secrets["snowflake"]["warehouse"],
+            database=st.secrets["snowflake"]["database"],
+            schema=st.secrets["snowflake"]["schema"]
+        )
+        return conn
+    except snowflake.connector.errors.ProgrammingError as e:
+        st.error(f"Programming error: {e}")
+    except snowflake.connector.errors.DatabaseError as e:
+        st.error(f"Database error: {e}")
+    except Exception as e:
+        st.error(f"Unexpected error: {e}")    
 
 # Fetch data from Snowflake
 def fetch_stock_prices():
