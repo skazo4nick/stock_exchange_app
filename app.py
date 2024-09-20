b24298a3043e0e6d04085846f14ec66fdc109066
Aleksey Vesyoliy <43518301+pirrencode@users.noreply.github.com>
2024-09-20 15:30:34 +0300

    Added description

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Streamlit app configuration
-st.title("Stock Prices Data Viewer v0.0.1")
+st.title("Stock Prices Data Viewer v0.0.2")
+st.write("This application displays the latest stocks from NASDAQ")
 
 # Snowflake connection details
 @st.cache_resource
